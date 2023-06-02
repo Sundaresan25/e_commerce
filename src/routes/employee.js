614afba7eb92cd24f701163b4dd8289a715442f1
sundar <sundarbgm98@gmail.com>
2023-06-02 14:49:28 +0530

    bug fix

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const EmployeeController = require("../controllers/EmployeeController");
-const upload = require("../middleware/upload");
+const upload = require("../middleware/Upload");
 const authenticate = require("../middleware/authenticate");
 
 router.get("/", authenticate, EmployeeController.index);
